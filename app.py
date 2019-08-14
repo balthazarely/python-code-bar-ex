c7460bf2a74af08ce2a2d9d6b38aed0955e052a5
Balthazar Ely <balthazarely@Balthazars-MacBook-Pro.local>
2019-08-14 17:00:03 -0600

    mostly finished project

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+#PART 1
+
 class Member():
     def __init__(self, name=""):
         self.name = name
@@ -8,7 +10,7 @@ class Student(Member):
     def __init__(self, name="", reasonToAttend=""):
         Member. __init__ (self)
         self.name = name
-        self.reasonToAttend = reasonToAttend   
+        self.reasonToAttend = reasonToAttend
 
 class Instructor(Member):
     def __init__(self, name="", bio="", skills=[""]):
@@ -16,20 +18,45 @@ class Instructor(Member):
         self.name = name
         self.bio = bio
         self.skills = skills
-    def addSkill(self, newSkill):
-        self.skills = self.skills + newSkill
-        print(f"{self.name} added {self.skills}")
+    def add_skill(self, newSkill):
+        self.skills.append(newSkill)
+        print(f"{self.name} added {newSkill} to her skillset")
 
        
 
-    
-ben = Student("ben", "I've been coding in Python for 5 years and want to share the love!")
-print("name: ", ben.name, " | reason: ", ben.reasonToAttend)
+ben = Student("ben", "I want to make a website!")
+sally = Student("sally", "I want to make an app!")
+# print("name: ", ben.name, " | reason: ", ben.reasonToAttend)
 
 alice = Instructor("alice", "I've been coding in Python for 5 years and wanted to share what i love!", ["Python", "Javascript", "C++"])
 print("name: ", alice.name, " | reason: ", alice.bio," | skills: ", alice.skills)
 
-alice.hello()
-alice.addSkill(["skateboarding", "underwater basket weaving"])
+# alice.hello()
+alice.add_skill("skateboarding")
+alice.add_skill("lazertag")
 print(alice.skills)
+
+# print(ben.__class__)
+
+
+class Workshop():
+    def __init__(self, subject="", date="", participants=[], instructors=[]):
+        self.subject = subject
+        self.date = date
+        self.participants = participants
+        self.instructors = instructors
+    def add_participant(self, addedperson):
+        if isinstance(addedperson, Student):
+            self.participants.append(addedperson)
+            print(f"{addedperson.name} was added to the class as a participant")
+        elif isinstance(addedperson, Instructor):
+            self.instructors.append(addedperson)
+            print(f"{addedperson.name} was added to the class as an instructors")    
+
+intro_to_dev = Workshop("development", "march 1st, 2020")
+
+intro_to_dev.add_participant(ben)
+intro_to_dev.add_participant(sally)
+intro_to_dev.add_participant(alice)
+
 
